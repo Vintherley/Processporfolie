907f73540f628bcaafda111d678e2ba388c27202
tanja <tanjamusen1@gmail.com>
2024-04-19 12:07:12 +0200

    ændringer

--- javascrip.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    if (document.getElementById("togglemenu").className == "content-width") {
-        let hamburger = document.getElementById("togglemenu");
-        hamburger.classList.add("hidden");
-    } else {
-        let hamburger = document.getElementById("togglemenu");
-        hamburger.classList.remove("hidden");
-    }   
-  }
